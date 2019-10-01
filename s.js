015df980a8048c0fc57dc62d6c237c7d2a9fade6
CGsama <CGsama@outlook.com>
2019-10-01 12:30:45 -0400

    Update s.js

--- s.js
+++ s.js
@@ -65,6 +65,7 @@ function app(req, res, https){
 	if(url.parse(req.url).pathname == '/shortener'){
 		var send = false;
 		if(url.parse(req.url).query != null){
+			console.log(url.parse(req.url).query.url);
 			if(url.parse(req.url).query.url != null){
 				let target = decodeURI(Buffer.from(url.parse(req.url,true).query.url, 'base64').toString('ascii'));
 				let base = url.parse(req.url,true).query.host
