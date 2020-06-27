8c240d5d25ccb5b156d3ea0a1b833e3caff4e812
CGsama <cgsama@outlook.com>
2020-06-27 19:36:00 -0400

    fix data folder

--- s.js
+++ s.js
@@ -41,6 +41,9 @@ try{
 		"privkey":"",
 		"fullchain":"",
 		"usessl":false};
+	if (!fs.existsSync('data')){
+		fs.mkdirSync('data');
+	}
 	db = new sqlite3.Database('data/url.db');
 }
 var domain = config.domain;
